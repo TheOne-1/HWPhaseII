e68b72819657a676b3f6f135259d56d78f49e588
zstrout <43109787+zstrout@users.noreply.github.com>
2019-09-05 22:35:57 +0800

    Revert "Revert "AP trunk angle algorithm done""
    This reverts commit 0ec4a14b5aba6e69fe01e3c9872a9c75af203c82.

--- 3_trunk/3_trunk.py
+++ 3_trunk/3_trunk.py
@@ -1,20 +1,26 @@
-from const import TRUNK_TRIALS
+from const import TRUNK_TRIALS, SUB_NAMES
 from ProcessorTrunk import ProcessorTrunk
 
 
 # define the output parameter here
-output_parameter_name = 'trunk_ml_angle'        # trunk_ap_angle or trunk_ml_angle
+output_parameter_name = 'trunk_ap_angle'        # trunk_ap_angle or trunk_ml_angle
 # define train set subject and trials here
 train = {}      # train dict is empty because you want to use a white box method
 # define test set subject and trials here
-test = {'190810LiuSensen': TRUNK_TRIALS, '190815WangHan': TRUNK_TRIALS}
-test = {'190810LiuSensen': TRUNK_TRIALS}
+test = {'190803LiJiayi': TRUNK_TRIALS, '190806SunDongxiao': TRUNK_TRIALS, '190806WangDianxin': TRUNK_TRIALS,
+            '190810LiuSensen': TRUNK_TRIALS, '190815WangHan': TRUNK_TRIALS}
+#test = {'190810LiuSensen': TRUNK_TRIALS}
 
 trunk_processor = ProcessorTrunk(train, test, 200, 'l', output_parameter_name, 'trunk', data_type=0,
                                  do_input_norm=False, do_output_norm=False)
-# trunk_processor.draw_subtrial_output_error_bar(trial_id=4)
-trunk_processor.prepare_train_test(trial_ids=[1], subtrial_ids=[4])
-trunk_processor.white_box_solution()
+#trunk_processor.draw_subtrial_output_error_bar(trial_id=4)
+
+for subject in test:
+    trunk_processor.calibrate_subject(subject_name=subject)
+    trunk_processor.prepare_train_test(subject_ids=[SUB_NAMES.index(subject)], subtrial_ids=None)
+    trunk_processor.white_box_solution()
+
+
 
 
 
