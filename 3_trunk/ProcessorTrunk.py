c3ab5f507328563131a1c4f2f99cf9c4678e625f
RTnhN <43109787+RTnhN@users.noreply.github.com>
2019-09-14 17:25:50 +0800

    Moving Trunk changes to master branch (#3)
    * Update Git Ignore
    
    * Update Git Ignore
    
    * Added FIR filter
    
    Cleaned Code

--- 3_trunk/ProcessorTrunk.py
+++ 3_trunk/ProcessorTrunk.py
@@ -7,126 +7,85 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from Evaluation import Evaluation
-from const import PROCESSED_DATA_PATH, SUB_NAMES
+from const import PROCESSED_DATA_PATH
+from scipy.signal import lfilter, butter
 
 
 class ProcessorTrunk(Processor):
     def __init__(self, train_sub_and_trials, test_sub_and_trials, sensor_sampling_fre, grf_side, param_name,
                  IMU_location, data_type, do_input_norm=True, do_output_norm=False, show_plots=True):
         super().__init__(train_sub_and_trials, test_sub_and_trials, sensor_sampling_fre, grf_side, param_name,
-                 IMU_location, data_type, do_input_norm=do_input_norm, do_output_norm=do_output_norm)
-        self.paramA = .1
-
-
-
-
-
+                         IMU_location, data_type, do_input_norm=do_input_norm, do_output_norm=do_output_norm,
+                         show_plots=show_plots)
+        self.comp_filter_mix_ratio_ml = .007
+        self.comp_filter_mix_ratio_ap = .01
+        self.filter_order = 4
+        self.filter_cutoff_freq = 6
 
     def convert_input_output(self, inputs, outputs, id_df, sampling_fre):
         if inputs is None:
             return None, None
+        
         trial_length = inputs.shape[0]
-        acc_filtered = np.zeros([trial_length, 3])
-        feature_0 = np.zeros([trial_length, 1])
-        bias = 0
+        estimated_angle = np.zeros([trial_length, 1])
+
+        filter_order = self.filter_order
+        cut_off_fre = self.filter_cutoff_freq
+        fre = cut_off_fre / (sampling_fre / 2)
+        b, a = butter(filter_order, fre, 'lowpass')
+        filtered_acc = lfilter(b, a, inputs[:, 0:3], axis=0)
+
         # see the id_df
         if self.param_name == "trunk_ap_angle":
-            comp_filter_param = self.paramA
-            exp_smooth_param = 0.02
-            scaling_factor = 1
-            # Initialization values for Comp Filter
+            comp_filter_param = self.comp_filter_mix_ratio_ap
             calibration_angle = self.cal_angle
             print(calibration_angle, end=',')
-            bias = calibration_angle * .4408 + 7.1926
-            #calibration_angle = 0
-            #bias = 0
-            print(bias, end=',')
-            acc_angle_prev = (-((np.arctan2(-inputs[0, 0], inputs[0, 2]) / np.pi * 180) - 90) - calibration_angle)/scaling_factor + bias
-            acc_prev = inputs[0, 0:3]
-
-            filter_result_angle_prev = acc_angle_prev
-
-            feature_0[0] = filter_result_angle_prev
-            acc_filtered[0, :] = acc_prev
-
+            bias = calibration_angle * .36 + 5
+            acc_angle_prev = (-((np.arctan2(np.linalg.norm(inputs[0, :]), inputs[0, 2]) / np.pi * 180) - 90) - 
+                              calibration_angle) + bias
+            estimated_angle[0] = acc_angle_prev
             for i in range(1, trial_length):
-                filtered_acc = acc_prev * (1-exp_smooth_param) + inputs[i, 0:3] * exp_smooth_param
+                acc_angle = (-((np.arctan2(np.linalg.norm(filtered_acc[i, :]), filtered_acc[i, 2]) / np.pi * 180) - 90)
+                             - calibration_angle) + bias
+                gyro_angle = estimated_angle[i-1] - (inputs[i, 4] / sampling_fre / np.pi * 180)
+                estimated_angle[i] = acc_angle * comp_filter_param + (1-comp_filter_param) * gyro_angle
 
-                acc_angle = (-((np.arctan2(-filtered_acc[0], filtered_acc[2]) / np.pi * 180) - 90) - calibration_angle)/scaling_factor + bias
-
-                gyro_angle = filter_result_angle_prev - (inputs[i, 4] / sampling_fre / np.pi * 180)
-
-                filter_result_angle = acc_angle * comp_filter_param + (1-comp_filter_param) * gyro_angle
-
-                acc_prev = filtered_acc
-                gyro_prev = inputs[i, 3:6]
-                acc_angle_prev = acc_angle
-                gyro_angle_prev = gyro_angle
-                filter_result_angle_prev = filter_result_angle
-                feature_0[i] = filter_result_angle
-                acc_filtered[i, :] = filtered_acc
         elif self.param_name == "trunk_ml_angle":
-            comp_filter_param = .007
-            exp_smooth_param = 0.015
-            # Initialization values for Comp Filter
+            comp_filter_param = self.comp_filter_mix_ratio_ml
             calibration_angle = self.cal_angle
-
+            bias = .73
             print(calibration_angle, end=',')
-            gyro_angle_prev = 0
-            acc_angle_prev = (np.arctan2(-inputs[0, 0], inputs[0, 1]) / np.pi * 180)/scaling_factor - 90 + calibration_angle
-            acc_prev = inputs[0, 0:3]
-            gyro_prev = inputs[0, 3:6]
-
-            filter_result_angle_prev = acc_angle_prev
-
-            feature_0[0] = filter_result_angle_prev - bias
-            acc_filtered[0, :] = acc_prev
-
+            acc_angle_prev = ((np.arctan2(np.linalg.norm(inputs[0, :]), inputs[0, 1]) / np.pi * 180) - 90) + \
+                calibration_angle - bias
+            estimated_angle[0] = acc_angle_prev
             for i in range(1, trial_length):
-                filtered_acc = acc_prev * (1 - exp_smooth_param) + inputs[i, 0:3] * exp_smooth_param
+                acc_angle = (np.arctan2(np.linalg.norm(filtered_acc[i, :]), filtered_acc[i, 1]) / np.pi * 180) - 90 + \
+                    calibration_angle - bias
+                gyro_angle = estimated_angle[i-1] - (inputs[i, 5] / sampling_fre / np.pi * 180)
+                estimated_angle[i] = acc_angle * comp_filter_param + (1 - comp_filter_param) * gyro_angle
 
-                acc_angle = (np.arctan2(-filtered_acc[0], filtered_acc[1]) / np.pi * 180)/scaling_factor - 90 + calibration_angle
-
-                gyro_angle = filter_result_angle_prev - (inputs[i, 5] / sampling_fre / np.pi * 180)
-
-                filter_result_angle = acc_angle * comp_filter_param + (1 - comp_filter_param) * gyro_angle
-
-                acc_prev = filtered_acc
-                gyro_prev = inputs[i, 3:6]
-                acc_angle_prev = acc_angle
-                gyro_angle_prev = gyro_angle
-                filter_result_angle_prev = filter_result_angle
-                feature_0[i] = filter_result_angle
-                acc_filtered[i, :] = filtered_acc
-
-        feature_0 = feature_0.reshape([-1, 1])
+        estimated_angle = estimated_angle.reshape([-1, 1])
         if self.show_plots:
             plt.figure()
             plt.plot(range(trial_length), inputs[:, 0], marker='o', markerfacecolor="blue")
-            plt.plot(range(trial_length), acc_filtered[:, 0], marker='.', markerfacecolor="orange")
+            plt.plot(range(trial_length), filtered_acc[:, 0], marker='.', markerfacecolor="orange")
             plt.show()
-            #
-            # plt.figure()
-            # plt.plot(range(trial_length), inputs[:, 4], marker='o', markerfacecolor="blue")
-            # plt.show()
 
             plt.figure()
-            plt.plot(range(trial_length), feature_0, marker='o', markerfacecolor="blue")
+            plt.plot(range(trial_length), estimated_angle, marker='o', markerfacecolor="blue")
             plt.plot(range(trial_length), outputs, marker='.', markerfacecolor="orange")
             plt.show()
-        return feature_0, outputs
+        return estimated_angle, outputs
 
     def calibrate_subject(self, subject_name):
 
         # get static trial data
         static_file = PROCESSED_DATA_PATH + '\\' + subject_name + '\\' + '200Hz\\static.csv'
         data_static = pd.read_csv(static_file)
-
-        # get marker offset
         column_names_acc = ["trunk_acc_x", "trunk_acc_y", "trunk_acc_z"]
-
         acc_data = data_static[column_names_acc]
         acc_vals = np.mean(acc_data)
+        acc_angle = 0
         if self.param_name == "trunk_ap_angle":
             acc_angle = -(np.arctan2(-acc_vals[0], acc_vals[2]) / np.pi * 180) + 90
         elif self.param_name == "trunk_ml_angle":
@@ -134,12 +93,10 @@ class ProcessorTrunk(Processor):
         self.cal_angle = acc_angle
 
     def white_box_solution(self):
-        # the algorithm
         y_pred = self._x_test
         R2, RMSE, mean_error = Evaluation._get_all_scores(self._y_test, y_pred)
 
         # show results
-
         if self.show_plots:
             plt.figure()
             plt.plot(self._y_test, y_pred, '.')
